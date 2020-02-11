bad15b127aaefaddc958a4e50393de322b7595de
kyubuns <kyubuns@gmail.com>
2020-02-11 23:20:01 +0900

    v1.0.0

---
+++ Assets/Development/PackageExporter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Export/Export Unity Package")]
+        public static void ExportUnityPackage()
+        {
+            var libraryDirectories = new[]
+            {
+                "Assets/BasicShapeSprite"
+            };
+
+            var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "BasicShapeSprite.unitypackage");
+
+            Debug.LogFormat("Export package to {0}", outputFilePath);
+            AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+            Debug.LogFormat("Success");
+        }
+    }
+}
